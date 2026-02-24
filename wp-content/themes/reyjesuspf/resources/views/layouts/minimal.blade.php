0de37cf107b46dfb59f4a2b8f221a807268cd627
TheYony <yonyjj0@gmail.com>
2026-02-24 16:11:57 -0400

    fix: Normalize doctype declaration in app and minimal templates

--- wp-content/themes/reyjesuspf/resources/views/layouts/minimal.blade.php
+++ wp-content/themes/reyjesuspf/resources/views/layouts/minimal.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+﻿<!doctype html>
 <html @php(language_attributes())>
   <head>
     <meta charset="utf-8">
@@ -7,6 +7,7 @@
     @php(wp_head())
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" type="image/png" href="@php(the_field('logo', 'option'))">
   </head>
 
   <body @php(body_class())>
@@ -36,3 +37,5 @@
     @php(wp_footer())
   </body>
 </html>
+
+
