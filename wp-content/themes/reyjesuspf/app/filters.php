5302978cd8b1086b700648abcdc410d6a0ecd249
Francisco Aular <franciscoaular18@gmail.com>
2026-03-02 13:11:54 -0400

    feat: Hide the WordPress admin bar for users with the subscriber role.

--- wp-content/themes/reyjesuspf/app/filters.php
+++ wp-content/themes/reyjesuspf/app/filters.php
@@ -14,3 +14,16 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/**
+ * Hide admin bar for subscribers.
+ *
+ * @param bool $show
+ * @return bool
+ */
+add_filter('show_admin_bar', function ($show) {
+    if (current_user_can('subscriber')) {
+        return false;
+    }
+    return $show;
+});
